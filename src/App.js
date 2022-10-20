a5f766df3a7dd3cdb82ab135765b0122ac8bb1ae
Jhosehp Rendon <jar@oiga.com>
2022-10-20 15:26:01 -0400

    Add base url to browserouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
         <Route path="/" exact element={<Home />}/>
         <Route path="/pago-confirmacion" exact element={<ConfirmacionPago />}/>
