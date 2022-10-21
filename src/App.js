250e579789a10fac1fa9b3fe5a20aecdda61f4af
Jhosehp Rendon <jar@oiga.com>
2022-10-21 14:16:29 -0400

    Add GA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React, {useEffect } from 'react';
 import Home from './components/Home';
 import ConfirmacionPago from './components/ConfirmacionPago'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ReactGA from 'react-ga';
+  const TRACKING_ID = "UA-152807118-1";
+  ReactGA.initialize(TRACKING_ID);
 
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
       <Router basename={process.env.PUBLIC_URL} >
         <Routes>
